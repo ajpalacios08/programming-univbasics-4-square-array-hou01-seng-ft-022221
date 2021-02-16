aa0ad0097a171944883e35016a7b8ca620778e16
Allen Palacios <apalacios1994@gmail.com>
2021-02-16 00:20:08 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,13 @@
-def square_array(numbers)
-  new_array = []
-  counter = 0 
-  while counter < numbers.length()
-  new_array.push(numbers[counter] * numbers[counter])
-  counter += 1
-  end 
-  return new_array
-end
+
   
+def square_array(array)
+  array = [1, 2, 3]
+  new_array = []
+  counter = 0
+  
+  while array.length.times do
+  new_array.push(array*2)
+  counter += 1
+end 
+new_array
+end
